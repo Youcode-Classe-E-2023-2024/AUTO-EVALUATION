cfab0fb62f5e2cb930d32dc21b744dac3dd9a3e9
MedHachami <moha.hach.9@gmail.com>
2024-01-06 21:58:00 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App bg-slate-600">
+      <h1 className='text-red-700'>Hell</h1>
+    </div>
+  );
+}
+
+export default App;
